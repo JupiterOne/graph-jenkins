68197e814beea672f97a2026f1e4c53b6c4b9d07
eXtremeX <viragsf@gmail.com>
2022-09-19 20:28:42 +0200

    Comment out build step related code

--- src/steps/build/converter.ts
+++ src/steps/build/converter.ts
@@ -1,62 +1,62 @@
-import {
-  createIntegrationEntity,
-  Entity,
-} from '@jupiterone/integration-sdk-core';
+// import {
+//   createIntegrationEntity,
+//   Entity,
+// } from '@jupiterone/integration-sdk-core';
 
-import { JenkinsBuild } from '../../types';
-import { Entities } from '../constants';
+// import { JenkinsBuild } from '../../types';
+// import { Entities } from '../constants';
 
-export function getBuildKey(id: string): string {
-  return `jenkins_build:${id}`;
-}
+// export function getBuildKey(id: string): string {
+//   return `jenkins_build:${id}`;
+// }
 
-function getStartedByUserId(build: JenkinsBuild) {
-  for (const action of build.actions) {
-    if (action.causes) {
-      for (const cause of action.causes) {
-        if (
-          cause.shortDescription.includes('Started by user') &&
-          cause.userId
-        ) {
-          return cause.userId;
-        }
-      }
-    }
-  }
-  return undefined;
-}
+// function getStartedByUserId(build: JenkinsBuild) {
+//   for (const action of build.actions) {
+//     if (action.causes) {
+//       for (const cause of action.causes) {
+//         if (
+//           cause.shortDescription.includes('Started by user') &&
+//           cause.userId
+//         ) {
+//           return cause.userId;
+//         }
+//       }
+//     }
+//   }
+//   return undefined;
+// }
 
-function getRemoteUrl(build: JenkinsBuild) {
-  for (const action of build.actions) {
-    if (
-      action._class &&
-      action._class === 'hudson.plugins.git.util.BuildData'
-    ) {
-      if (action.remoteUrls.length > 0) {
-        return action.remoteUrls[0];
-      }
-    }
-  }
-  return undefined;
-}
+// function getRemoteUrl(build: JenkinsBuild) {
+//   for (const action of build.actions) {
+//     if (
+//       action._class &&
+//       action._class === 'hudson.plugins.git.util.BuildData'
+//     ) {
+//       if (action.remoteUrls.length > 0) {
+//         return action.remoteUrls[0];
+//       }
+//     }
+//   }
+//   return undefined;
+// }
 
-export function createBuildEntity(build: JenkinsBuild): Entity {
-  const startedByUserId = getStartedByUserId(build);
-  const remoteUrl = getRemoteUrl(build);
+// export function createBuildEntity(build: JenkinsBuild): Entity {
+//   const startedByUserId = getStartedByUserId(build);
+//   const remoteUrl = getRemoteUrl(build);
 
-  return createIntegrationEntity({
-    entityData: {
-      source: build,
-      assign: {
-        _type: Entities.BUILD._type,
-        _class: Entities.BUILD._class,
-        _key: getBuildKey(build.url),
-        id: build.url,
-        webLink: build.url,
-        name: build.url,
-        remoteUrl,
-        startedByUserId,
-      },
-    },
-  });
-}
+//   return createIntegrationEntity({
+//     entityData: {
+//       source: build,
+//       assign: {
+//         _type: Entities.BUILD._type,
+//         _class: Entities.BUILD._class,
+//         _key: getBuildKey(build.url),
+//         id: build.url,
+//         webLink: build.url,
+//         name: build.url,
+//         remoteUrl,
+//         startedByUserId,
+//       },
+//     },
+//   });
+// }
--- src/steps/build/index.ts
+++ src/steps/build/index.ts
@@ -1,105 +1,105 @@
-import {
-  createDirectRelationship,
-  IntegrationStep,
-  IntegrationStepExecutionContext,
-  RelationshipClass,
-  getRawData,
-} from '@jupiterone/integration-sdk-core';
+// import {
+//   createDirectRelationship,
+//   IntegrationStep,
+//   IntegrationStepExecutionContext,
+//   RelationshipClass,
+//   getRawData,
+// } from '@jupiterone/integration-sdk-core';
 
-import { createAPIClient } from '../../client';
-import { IntegrationConfig } from '../../config';
-import { Entities, Relationships, Steps } from '../constants';
-import { JenkinsBuild, JenkinsJob } from '../../types';
-import { createBuildEntity } from './converter';
-import { getUserKey } from '../user/converter';
+// import { createAPIClient } from '../../client';
+// import { IntegrationConfig } from '../../config';
+// import { Entities, Relationships, Steps } from '../constants';
+// import { JenkinsBuild, JenkinsJob } from '../../types';
+// import { createBuildEntity } from './converter';
+// import { getUserKey } from '../user/converter';
 
-export async function fetchBuilds({
-  instance,
-  jobState,
-  logger,
-}: IntegrationStepExecutionContext<IntegrationConfig>) {
-  const apiClient = createAPIClient(instance.config);
+// export async function fetchBuilds({
+//   instance,
+//   jobState,
+//   logger,
+// }: IntegrationStepExecutionContext<IntegrationConfig>) {
+//   const apiClient = createAPIClient(instance.config);
 
-  await jobState.iterateEntities(
-    { _type: Entities.JOB._type },
-    async (jobEntity) => {
-      const job = getRawData<JenkinsJob>(jobEntity);
-      if (!job) {
-        logger.warn(
-          { _key: jobEntity._key },
-          'Could not get raw data for job entity',
-        );
-        return;
-      }
+//   await jobState.iterateEntities(
+//     { _type: Entities.JOB._type },
+//     async (jobEntity) => {
+//       const job = getRawData<JenkinsJob>(jobEntity);
+//       if (!job) {
+//         logger.warn(
+//           { _key: jobEntity._key },
+//           'Could not get raw data for job entity',
+//         );
+//         return;
+//       }
 
-      await apiClient.iterateBuilds(async (build) => {
-        const buildDetails = await apiClient.fetchBuildDetails(build.url);
-        const buildEntity = await jobState.addEntity(
-          createBuildEntity(buildDetails),
-        );
+//       await apiClient.iterateBuilds(async (build) => {
+//         const buildDetails = await apiClient.fetchBuildDetails(build.url);
+//         const buildEntity = await jobState.addEntity(
+//           createBuildEntity(buildDetails),
+//         );
 
-        await jobState.addRelationship(
-          createDirectRelationship({
-            _class: RelationshipClass.HAS,
-            from: jobEntity,
-            to: buildEntity,
-          }),
-        );
-      }, job.url);
-    },
-  );
-}
+//         await jobState.addRelationship(
+//           createDirectRelationship({
+//             _class: RelationshipClass.HAS,
+//             from: jobEntity,
+//             to: buildEntity,
+//           }),
+//         );
+//       }, job.url);
+//     },
+//   );
+// }
 
-export async function buildUserAndBuildRelationships({
-  jobState,
-  logger,
-}: IntegrationStepExecutionContext<IntegrationConfig>) {
-  await jobState.iterateEntities(
-    { _type: Entities.BUILD._type },
-    async (buildEntity) => {
-      const build = getRawData<JenkinsBuild>(buildEntity);
-      if (!build) {
-        logger.warn(
-          { _key: buildEntity._key },
-          'Could not get raw data for device entity',
-        );
-        return;
-      }
-      const userId = buildEntity.startedByUserId;
+// export async function buildUserAndBuildRelationships({
+//   jobState,
+//   logger,
+// }: IntegrationStepExecutionContext<IntegrationConfig>) {
+//   await jobState.iterateEntities(
+//     { _type: Entities.BUILD._type },
+//     async (buildEntity) => {
+//       const build = getRawData<JenkinsBuild>(buildEntity);
+//       if (!build) {
+//         logger.warn(
+//           { _key: buildEntity._key },
+//           'Could not get raw data for device entity',
+//         );
+//         return;
+//       }
+//       const userId = buildEntity.startedByUserId;
 
-      if (userId) {
-        const isUserEntity = await jobState.findEntity(
-          getUserKey(userId.toString()),
-        );
+//       if (userId) {
+//         const isUserEntity = await jobState.findEntity(
+//           getUserKey(userId.toString()),
+//         );
 
-        if (isUserEntity)
-          await jobState.addRelationship(
-            createDirectRelationship({
-              _class: RelationshipClass.HAS,
-              from: isUserEntity,
-              to: buildEntity,
-            }),
-          );
-      }
-    },
-  );
-}
+//         if (isUserEntity)
+//           await jobState.addRelationship(
+//             createDirectRelationship({
+//               _class: RelationshipClass.HAS,
+//               from: isUserEntity,
+//               to: buildEntity,
+//             }),
+//           );
+//       }
+//     },
+//   );
+// }
 
-export const buildSteps: IntegrationStep<IntegrationConfig>[] = [
-  {
-    id: Steps.BUILD,
-    name: 'Fetch Build',
-    entities: [Entities.BUILD],
-    relationships: [Relationships.JOB_HAS_BUILD],
-    dependsOn: [Steps.JOB],
-    executionHandler: fetchBuilds,
-  },
-  {
-    id: Steps.BUILD_USER_BUILD_RELATIONSHIPS,
-    name: 'Build User and Build Relationships',
-    entities: [],
-    relationships: [Relationships.USER_HAS_BUILD],
-    dependsOn: [Steps.BUILD, Steps.USER],
-    executionHandler: buildUserAndBuildRelationships,
-  },
-];
+// export const buildSteps: IntegrationStep<IntegrationConfig>[] = [
+//   {
+//     id: Steps.BUILD,
+//     name: 'Fetch Build',
+//     entities: [Entities.BUILD],
+//     relationships: [Relationships.JOB_HAS_BUILD],
+//     dependsOn: [Steps.JOB],
+//     executionHandler: fetchBuilds,
+//   },
+//   {
+//     id: Steps.BUILD_USER_BUILD_RELATIONSHIPS,
+//     name: 'Build User and Build Relationships',
+//     entities: [],
+//     relationships: [Relationships.USER_HAS_BUILD],
+//     dependsOn: [Steps.BUILD, Steps.USER],
+//     executionHandler: buildUserAndBuildRelationships,
+//   },
+// ];
